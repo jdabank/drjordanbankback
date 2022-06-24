fbbbb4d18a188a146a5fc6430d8ff3c754e607d1
Jordan Bank <jordanbank@Jordans-MacBook-Air.local>
2022-06-24 11:06:21 -0400

    added new field to backend

--- drjordanbank_api/appointments/models.py
+++ drjordanbank_api/appointments/models.py
@@ -15,6 +15,20 @@ class Appointment(models.Model):
         JERUSALEM_SYNDROME = ('Jerusalem Syndrome')
         ATE_TOO_MUCH = ('Ate too much')
 
+    class Physician(models.TextChoices):
+        JORDAN_BANK = ('Jordan Bank')
+        MEILING_LI = ('Meiling Li')
+        TYGER_SALTERS = ('Tyger Salters')
+        OFORI_MANSON = ('Ofori Manson')
+        KEVIN_HA = ('Kevin Ha')
+        JEAN_ENG = ('Jean Eng')
+        RAFAY_HASSAN = ('Rafay Hassan')
+
+    physician = models.CharField(
+        max_length=50,
+        choices = Physician.choices,
+        default = Physician.JORDAN_BANK
+        )
     name = models.CharField(max_length=50)
     age = models.IntegerField()
     reason = models.CharField(
